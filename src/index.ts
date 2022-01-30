ce12ad23935a3e38ab0ea64f2e5b15e0a4a8bc7f
glenchiridza <glenchiridz@gmail.com>
2022-01-31 00:49:06 +0200

    convert dom elements picker done

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+
 function Sum(){
     return 5 + 6;
 }
@@ -28,22 +29,33 @@ to_currency?.addEventListener('change',(event:any)=>{
 
 input_amt?.addEventListener('input',updateValue);
 
+
+
 function updateValue(event:any){
     input_amount = event.target.value;
 }
 
-btn_convert?.addEventListener('click',values);
+btn_convert?.addEventListener('click',fetchResult);
 
 
-function values(){
+function fetchResult(){
     console.log(input_amount)
     console.log(resultTo)
     console.log(resultFrom)
-    var add_to_api_url:string = base_url+`convert?amount=${input_amount}&from=${resultFrom}&to=${resultTo}`;
-    console.log(add_to_api_url);
+    if(input_amount){
+        var add_to_api_url:string = base_url+`convert?amount=${input_amount}&from=${resultFrom}&to=${resultTo}`;
+        console.log(add_to_api_url);
+    }else{
+
+        console.log("amount is required");
+    }
 }
 
+function displayResult(currency:any){
+    final_val = currency.info.rate;
+}
 
+// console.log(CODES.CURRENCY_CODES[0]);
 
 
 // function getResults(){
@@ -64,14 +76,3 @@ function values(){
 
 
 
-//currency codes array
-["AED","AFN","ALL","AMD","ANG","AOA","ARS","AUD","AWG","AZN","BAM","BBD","BDT","BGN","BHD",
-"BIF","BMD","BND","BOB","BRL","BSD","BTC","BTN","BWP","BYN","BZD","CAD","CDF","CHF","CLF","CLP","CNH","CNY","COP","CRC","CUC","CUP","CVE",
-"CZK","DJF","DKK","DOP","DZD","EGP","ERN","ETB","EUR","FJD","FKP","GBP","GEL","GGP","GHS","GIP","GMD",
-"GNF","GTQ","GYD","HKD","HNL","HRK","HTG","HUF","IDR","ILS","IMP","INR","IQD","IRR","ISK","JEP","JMD",
-"JOD","JPY","KES","KGS","KHR","KMF","KPW","KRW","KWD","KYD","KZT","LAK","LBP","LKR","LRD","LSL","LYD",
-"MAD","MDL","MGA","MKD","MMK","MNT","MOP","MRU","MUR","MVR","MWK","MXN","MYR","MZN","NAD","NGN","NIO",
-"NOK","NPR","NZD","OMR","PAB","PEN","PGK","PHP","PKR","PLN","PYG","QAR","RON","RSD","RUB","RWF","SAR",
-"SBD","SCR","SDG","SEK","SGD","SHP","SLL","SOS","SRD","SSP","STD","STN","SVC","SYP","SZL","THB","TJS",
-"TMT","TND","TOP","TRY","TTD","TWD","TZS","UAH","UGX","USD","UYU","UZS","VES","VND","VUV","WST","XAF",
-"XAG","XAU","XCD","XDR","XOF","XPD","XPF","XPT","YER","ZAR","ZMW","ZWL"]
